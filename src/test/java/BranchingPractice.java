240d22c004e49bdf5463d3b88fe7dc4455f6e775
Madina <danaeva.madina@gmail.com>
2021-05-25 19:24:02 -0400

    shared on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,5 +1,7 @@
 public class BranchingPractice {
     public static void main(String[] args) {
-
+    //create new project
+        //enable version control
+        //added .gitignore with idea
     }
 }
